5feedb04617fae2bd32a517d959ff16fcb1d3640
earth <divine.icbm@gmail.com>
2024-04-02 17:24:01 -0500

    add more metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,22 @@ import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Home",
+  creator: "Jace Royer",
+  openGraph: {
+    title: "jroyvibes",
+    description: "Jace Royer's portfolio, for all things past, present, and future.",
+    url: "https://jroyvibes.vercel.app/",
+    siteName: "jroyvibes",
+    images: [
+      {
+        url: "https://imgur.com/lB2MaTl",
+        width: 800,
+        height: 800
+      }
+    ],
+    locale: "en_US",
+    type: "website"
+  }
 };
 
 export default HomePage;
